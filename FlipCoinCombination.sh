7bd95573f3115c1b87a76bbc75a34e5744fc52b8
Avinash <sharma.avinash96@gmail.com>
2020-08-05 20:27:27 +0530

    store singlet in dictionary and display singlet percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,8 +1,39 @@
 #!/bin/bash -x
+
+declare -A coin
+
+totalFlip=200
+count=0
+while (( $count < totalFlip ))
+do
 coinFlip=$((RANDOM%2))
-if [[ $coinFlip -eq 1 ]]
-then
-        echo "Heads"
-else
-        echo "Tails"
+
+   if [[ $coinFlip -eq 1 ]]
+   then
+      echo "Heads"
+      if [ -z "${coin[H]}" ]
+      then
+      coin[H]=1
+      else
+      coin[H]=$((${coin[H]}+1))
+      fi
+   else
+      echo "Tails"
+      if [ -z "${coin[T]}" ]
+      then
+      coin[T]=1
+      else
+      coin[T]=$((${coin[T]}+1))
+   fi
 fi
+((count++))
+done
+
+echo "H/T: "${!coin[@]}
+echo "Value: "${coin[@]}
+
+Hper=`awk "BEGIN {print (${coin[H]}/$totalFlip)*100}"`
+Tper=`awk "BEGIN {print (${coin[T]}/$totalFlip)*100}"`
+echo "Head Percentage: $Hper% "
+echo "Tail Percentage: $Tper%"
+
